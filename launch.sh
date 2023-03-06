71de4fa2c7f22dca49f6b56ed31092474a9e4b44
Mohammad Wibowo <mohammadwibowo@mwbowo-macpro.local>
2023-03-07 03:35:25 +0700

    Update 1.2

--- launch.sh
+++ launch.sh
@@ -1,10 +1,24 @@
+#!/bin/bash
+
+# Update the apt package index
 sudo apt update
+
+# Install packages to allow apt to use a repository over HTTPS
 sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
-/usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable Docker repository
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the apt package index again
 sudo apt update
+
+# Install the latest version of Docker Engine and containerd
 sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo docker compose up -d
+
+# Run docker compose 
+sudo docker compose up -d 
